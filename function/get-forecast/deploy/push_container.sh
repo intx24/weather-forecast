7a979834f96a1699994eaf97b9ef9691deaeac7f
Tomohiro Yoshida <rollwithit0117@gmail.com>
2021-02-23 15:44:17 +0900

    タグ名修正

--- function/get-forecast/deploy/push_container.sh
+++ function/get-forecast/deploy/push_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 AWS_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text)
-docker tag local:list-channels $AWS_ACCOUNT.dkr.ecr.ap-northeast-1.amazonaws.com/weather-forecast-repo:list-channels
-docker push $AWS_ACCOUNT.dkr.ecr.ap-northeast-1.amazonaws.com/weather-forecast-repo:list-channels
+docker tag local:get-forecast $AWS_ACCOUNT.dkr.ecr.ap-northeast-1.amazonaws.com/weather-forecast-repo:get-forecast
+docker push $AWS_ACCOUNT.dkr.ecr.ap-northeast-1.amazonaws.com/weather-forecast-repo:get-forecast
